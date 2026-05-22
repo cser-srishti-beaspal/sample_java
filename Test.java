ac3005108c56d62800798fcce9b91f740207bb35
cser-srishti-beaspal <cser.srishti.beaspal@gmail.com>
2026-05-22 12:10:09 +0530

    addd

--- Test.java
+++ Test.java
@@ -2,10 +2,11 @@ public class Test {
 
   static void add(){
 
-    int a=48;
+    int a=40;
     int b=65;
-    int result = a*b;
-    System.out.println(" NO ERROR ");
+    int result = a+b;
+    System.out.println(" NO ERROR Please ");
+    
     System.out.println("26 me toh duniya hi khtm hai");
     System.out.println("The sum is -  " +result);
     
