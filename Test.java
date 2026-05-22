bdcba2bb5493fdda6e6a6cf459909265705a600a
cser-srishti-beaspal <cser.srishti.beaspal@gmail.com>
2026-05-22 12:15:42 +0530

    updates

--- Test.java
+++ Test.java
@@ -3,8 +3,8 @@ public class Test {
   static void add(){
 
     int a=40;
-    int b=65;
-    int result = a+b;
+    int b=45;
+    int result = a*b;
     System.out.println(" NO ERROR Please ");
     
     System.out.println("26 me toh duniya hi khtm hai");
